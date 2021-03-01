38f36ec1998698a23847ce761ddb483998bec9c7
Eli A Zupke <Trainzack@users.noreply.github.com>
2021-03-01 15:30:31 -0800

    Resturcture project into multi-module project
    Move the main program out of the main maven module into a submodule
    Create a new module for izPack
    Remove unneeded 'files' directory

---
+++ librario/src/test/java/org/librario/EmptyTest.java
@@ -0,0 +1,17 @@
+package org.librario;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Empty unit test.
+ */
+public class EmptyTest {
+
+	@Test
+	public void emptyUnitTest() {
+		assertTrue(true);
+	}
+
+}
---
+++ librario/src/test/java/org/librario/InitializationIT.java
@@ -0,0 +1,42 @@
+package org.librario;
+
+import static org.junit.Assert.assertTrue;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import io.spotnext.core.CoreInit;
+import io.spotnext.core.testing.AbstractIntegrationTest;
+import io.spotnext.core.testing.IntegrationTest;
+import io.spotnext.instrumentation.DynamicInstrumentationLoader;
+import org.librario.Init;
+
+/**
+ * This integration test checks if the module has been initialized properly.
+ */
+@IntegrationTest(initClass = Init.class)
+@SpringBootTest(classes = { Init.class, CoreInit.class })
+public class InitializationIT extends AbstractIntegrationTest {
+
+	@Autowired
+	Init init;
+
+	@Override
+	protected void prepareTest() {
+		//
+	}
+
+	@Override
+	protected void teardownTest() {
+		//
+	}
+
+	@Test
+	public void moduleInitialized() {
+		assertTrue(Init.class.getSimpleName() + " not initialized", init.isAlreadyInitialized());
+		assertTrue("Instrumentation not initiazed", DynamicInstrumentationLoader.isInstrumentationAvailable());
+	}
+
+}
